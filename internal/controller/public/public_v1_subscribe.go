4ff89d8b48b24b0b88fe568368f74d9962de2ecc
pings <ssrpub@gmail.com>
2024-03-19 16:20:20 +0800

    clash订阅

--- internal/controller/public/public_v1_subscribe.go
+++ internal/controller/public/public_v1_subscribe.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 
 	v1 "gov2panel/api/public/v1"
@@ -12,6 +13,7 @@ import (
 	"gov2panel/internal/service"
 	"gov2panel/internal/utils"
 
+	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/util/gconv"
 )
@@ -43,6 +45,7 @@ func (c *ControllerV1) Subscribe(ctx context.Context, req *v1.SubscribeReq) (res
 		result = ShadowsocksSub(serviceArr, user)
 	case "clash":
 		result = ClashSub(serviceArr, user)
+		ghttp.RequestFromCtx(ctx).Response.WriteExit([]byte(result))
 	default:
 		result = ""
 	}
@@ -116,14 +119,25 @@ func base64Sub(serviceArr []*entity.V2ProxyService, user *entity.V2User) (result
 			)
 
 		case "ss2022":
+			ssPasswd := user.Uuid
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-128-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:16]),
+				)
+			}
+
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-256-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:32]),
+				)
+			}
+
 			// ss://base64(加密方式:密码)@地址:端口#别名
 			// ss://OjY4ZDJjNTFmLTUzMTEtNDc2MS1hYTNhLTllNDg1MmYzMGYyNQ==@127.0.0.1:9996#ss2022
 			result = result + fmt.Sprintf(
 				"%s://%s@%s:%s#%s\n",
 				"ss",
-				base64.StdEncoding.EncodeToString(
-					gconv.Bytes(gconv.String(serviceJson["encryption"])+":"+user.Uuid),
-				),
+				ssPasswd,
 				service.Host,
 				service.Port,
 				service.Name,
@@ -310,19 +324,54 @@ func NekoBoxSub(serviceArr []*entity.V2ProxyService, user *entity.V2User) (resul
 
 // Shadowsocks订阅
 func ShadowsocksSub(serviceArr []*entity.V2ProxyService, user *entity.V2User) (result string) {
+	result = result + fmt.Sprintf(
+		"%s://%s@%s:%s#%s\n",
+		"ss",
+		base64.StdEncoding.EncodeToString(
+			gconv.Bytes("aes-128-gcm:"+user.Uuid),
+		),
+		"127.0.0.1",
+		"80",
+		"套餐到期："+user.ExpiredAt.Format("Y-m-d H:i"),
+	)
+
+	result = result + fmt.Sprintf(
+		"%s://%s@%s:%s#%s\n",
+		"ss",
+		base64.StdEncoding.EncodeToString(
+			gconv.Bytes("aes-128-gcm:"+user.Uuid),
+		),
+		"127.0.0.1",
+		"80",
+		"剩余流量："+fmt.Sprintf("%.2f GB", utils.BytesToGB(user.TransferEnable-user.U-user.D)),
+	)
+
 	for _, service := range serviceArr {
+		service.Host = strings.ReplaceAll(service.Host, "$uuid$", user.Uuid)
+
 		serviceJson := make(map[string]interface{})
 		json.Unmarshal([]byte(service.ServiceJson), &serviceJson)
 		switch strings.Split(service.Agreement, "/")[1] {
 		case "ss2022":
+			ssPasswd := user.Uuid
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-128-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:16]),
+				)
+			}
+
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-256-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:32]),
+				)
+			}
+
 			// ss://base64(加密方式:密码)@地址:端口#别名
 			// ss://OjY4ZDJjNTFmLTUzMTEtNDc2MS1hYTNhLTllNDg1MmYzMGYyNQ==@127.0.0.1:9996#ss2022
 			result = result + fmt.Sprintf(
 				"%s://%s@%s:%s#%s\n",
 				"ss",
-				base64.StdEncoding.EncodeToString(
-					gconv.Bytes(gconv.String(serviceJson["encryption"])+":"+user.Uuid),
-				),
+				ssPasswd,
 				service.Host,
 				service.Port,
 				service.Name,
@@ -335,6 +384,120 @@ func ShadowsocksSub(serviceArr []*entity.V2ProxyService, user *entity.V2User) (r
 
 // Clash订阅
 func ClashSub(serviceArr []*entity.V2ProxyService, user *entity.V2User) (result string) {
+
+	content, err := os.ReadFile("./manifest/config/clash.yaml")
+	if err != nil {
+		result = "读取文件错误./manifest/config/clash.yaml，错误：" + err.Error()
+		return
+	}
+
+	result = string(content)
+
+	nodeNameArr := make([]string, 0)
+
+	nodeInfoArr := make([]string, 0)
+
+	for _, service := range serviceArr {
+		service.Host = strings.ReplaceAll(service.Host, "$uuid$", user.Uuid)
+		serviceJson := make(map[string]interface{})
+		json.Unmarshal([]byte(service.ServiceJson), &serviceJson)
+
+		nodeNameArr = append(nodeNameArr, "      - "+service.Name)
+
+		d := map[string]interface{}{
+			"name":   service.Name,
+			"port":   service.Port,
+			"server": service.Host,
+		}
+
+		switch strings.Split(service.Agreement, "/")[1] {
+		case "vmess":
+			d["type"] = "vmess"
+			d["uuid"] = user.Uuid
+			d["alterId"] = 0
+			d["cipher"] = gconv.String(serviceJson["scy"]) //加密方式
+			if gconv.String(serviceJson["tls"]) == "tls" { //tls
+				d["tls"] = true
+				d["skip-cert-verify"] = false
+			}
+			d["servername"] = gconv.String(serviceJson["sni"])
+			d["network"] = gconv.String(serviceJson["net"]) //传输协议
+			switch gconv.String(serviceJson["net"]) {       //传输协议
+			case "ws":
+				d["ws-path"] = ""
+				d["ws-headers"] = map[string]interface{}{
+					"Host": gconv.String(serviceJson["host"]),
+				}
+			}
+
+			if gconv.String(serviceJson["type"]) == "http" { //伪装类型
+				d["network"] = "http"
+				d["http-opts"] = map[string]interface{}{
+					"method":  "GET",
+					"path":    []string{gconv.String(serviceJson["path"])},
+					"headers": map[string]interface{}{"Host": []string{gconv.String(serviceJson["host"])}},
+				}
+			}
+
+		case "vless":
+			d["type"] = "vless"
+			d["uuid"] = user.Uuid
+			d["alterId"] = 0
+			d["cipher"] = gconv.String(serviceJson["scy"]) //加密方式
+			if gconv.String(serviceJson["tls"]) == "tls" { //tls
+				d["tls"] = true
+				d["skip-cert-verify"] = false
+			}
+			d["servername"] = gconv.String(serviceJson["sni"])
+			d["network"] = gconv.String(serviceJson["net"]) //传输协议
+			switch gconv.String(serviceJson["net"]) {       //传输协议
+			case "ws":
+				d["ws-path"] = ""
+				d["ws-headers"] = map[string]interface{}{
+					"Host": gconv.String(serviceJson["host"]),
+				}
+			}
+
+			if gconv.String(serviceJson["type"]) == "http" { //伪装类型
+				d["network"] = "http"
+				d["http-opts"] = map[string]interface{}{
+					"method":  "GET",
+					"path":    []string{gconv.String(serviceJson["path"])},
+					"headers": map[string]interface{}{"Host": []string{gconv.String(serviceJson["host"])}},
+				}
+			}
+		case "ss2022":
+			ssPasswd := user.Uuid
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-128-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:16]),
+				)
+			}
+
+			if gconv.String(serviceJson["encryption"]) == "2022-blake3-aes-256-gcm" {
+				ssPasswd = base64.StdEncoding.EncodeToString(
+					gconv.Bytes(gconv.String(serviceJson["encryption"]) + ":" + user.Uuid[0:32]),
+				)
+			}
+			d["type"] = "ss"
+			d["cipher"] = gconv.String(serviceJson["encryption"]) //加密方式
+			d["password"] = ssPasswd                              //密码
+
+		case "trojan":
+			d["type"] = "trojan"
+			d["password"] = user.Uuid
+			d["sni"] = gconv.String(serviceJson["sni"])
+			d["alpn"] = []string{gconv.String(serviceJson["alpn"])}
+			d["skip-cert-verify"] = false
+		}
+
+		j, _ := gjson.EncodeString(d)
+		fmt.Println(string(j))
+		nodeInfoArr = append(nodeInfoArr, "  - "+j)
+	}
+
+	result = strings.ReplaceAll(result, "{{node_info}}", strings.Join(nodeInfoArr, "\n"))
+	result = strings.ReplaceAll(result, "{{node_name}}", strings.Join(nodeNameArr, "\n"))
 
 	return
 }
